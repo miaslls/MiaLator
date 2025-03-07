74c4a3515e7f3c8db8274b3fe00aa2dac86f2b99
miaslls <dev@camilasalles.dev>
2025-03-07 19:27:48 -0300

    feat: logic 0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,95 @@
 import './App.css';
 
+import { useState } from 'react';
+
+type Operator = '+' | '-' | '*' | '/';
+
 function App() {
+  const [firstOperand, setFirstOperand] = useState<number | undefined>(undefined);
+  const [operator, setOperator] = useState<Operator | undefined>(undefined);
+  const [secondOperand, setSecondOperand] = useState<number | undefined>(undefined);
+
+  const clear = () => {
+    setFirstOperand(undefined);
+    setOperator(undefined);
+    setSecondOperand(undefined);
+  };
+
+  const calculate = () => {
+    if (!firstOperand || !operator) return;
+    if (secondOperand === undefined) {
+      setOperator(undefined);
+      return;
+    }
+
+    let result: number;
+
+    if (operator === '+') result = firstOperand + secondOperand;
+    else if (operator === '-') result = firstOperand - secondOperand;
+    else if (operator === '*') result = firstOperand * secondOperand;
+    else {
+      if (secondOperand === 0) {
+        alert('THE UNIVERSE EXPLODED!');
+        clear();
+        return;
+      } else result = firstOperand / secondOperand;
+    }
+
+    setFirstOperand(result);
+    setOperator(undefined);
+    setSecondOperand(undefined);
+  };
+
+  const clickNumber = (num: number) => {
+    if (firstOperand === undefined) setFirstOperand(num);
+    else if (!operator) setFirstOperand((prev) => Number(`${prev}${num}`));
+    else if (secondOperand === undefined) setSecondOperand(num);
+    else setSecondOperand((prev) => Number(`${prev}${num}`));
+  };
+
+  const clickOperator = (op: Operator) => {
+    if (firstOperand === undefined) setFirstOperand(0);
+    if (secondOperand === undefined) setOperator(op);
+  };
+
   return (
     <>
       <h1>MiaLator</h1>
       <div className='calculator'>
-        <div className='display'>0</div>
+        <div className='display'>
+          {firstOperand ?? 0}
+          {operator ? ` ${operator}` : ''}
+          {secondOperand !== undefined ? ` ${secondOperand}` : ''}
+        </div>
         <div className='buttons'>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='operator'>/</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button className='operator'>*</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className='operator'>-</button>
-          <button className='zero'>0</button>
-          <button>C</button>
-          <button className='operator'>+</button>
-          <button className='equals'>=</button>
+          <button onClick={() => clickNumber(7)}>7</button>
+          <button onClick={() => clickNumber(8)}>8</button>
+          <button onClick={() => clickNumber(9)}>9</button>
+          <button className='operator' onClick={() => clickOperator('/')}>
+            /
+          </button>
+          <button onClick={() => clickNumber(4)}>4</button>
+          <button onClick={() => clickNumber(5)}>5</button>
+          <button onClick={() => clickNumber(6)}>6</button>
+          <button className='operator' onClick={() => clickOperator('*')}>
+            *
+          </button>
+          <button onClick={() => clickNumber(1)}>1</button>
+          <button onClick={() => clickNumber(2)}>2</button>
+          <button onClick={() => clickNumber(3)}>3</button>
+          <button className='operator' onClick={() => clickOperator('-')}>
+            -
+          </button>
+          <button className='zero' onClick={() => clickNumber(0)}>
+            0
+          </button>
+          <button onClick={clear}>C</button>
+          <button className='operator' onClick={() => clickOperator('+')}>
+            +
+          </button>
+          <button className='equals' onClick={calculate}>
+            =
+          </button>
         </div>
       </div>
       <p style={{ textAlign: 'right', paddingRight: '10px' }}>0.1.0-alpha</p>
